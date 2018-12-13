c04b840e0b6f81b7a85e7a2deb985c61c4ed56a7
tyarinskiy <tyarin@list.ru>
2018-12-14 00:13:21 +0300

    Update signalka_moovSensor.ino
    proj_for_max

--- mini_proj/signalka_moovSensor/signalka_moovSensor.ino
+++ mini_proj/signalka_moovSensor/signalka_moovSensor.ino
@@ -1,34 +1,39 @@
 /* 
+ *  сигналка с сенсором движеня, пищалкой и мигалкой из светодиодов
 */
 
 int movPin = 3; //контакт к которму подключена кнопка
 
-int ledPin = 12;  //контакт к которму подключен светодиод
+int ledredPin = 12;  //контакт к которму подключен светодиод
+int ledbluePin = 11; // светодиод сигнализирующий постановку на охрану
 int activBuz = 7; // активный динамик
 
 void setup() 
 {
   pinMode(movPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledredPin, OUTPUT);
   pinMode(activBuz, OUTPUT);
+  pinMode(ledbluePin, OUTPUT);
 }
 
 void loop() 
 {
+  
   if (digitalRead(movPin) == HIGH) // если считываем с 8 контакта 1 
   {
-    digitalWrite(ledPin, HIGH);       // то считаем, что на 12м контакте (диоде) 1
-//    digitalWrite(activBuz, HIGH);
+    digitalWrite(ledredPin, HIGH);       // то считаем, что на 12м контакте (диоде) 1
+    digitalWrite(ledbluePin, LOW);    
     tone(activBuz, 400);
     delay(300);
-//    tone(activBuz, 800);
-//    delay(300);  
+    digitalWrite(ledredPin, LOW);
+    digitalWrite(ledbluePin, HIGH);  
     tone(activBuz, 1200);
     delay(300);  
   }
   else
   {
-    digitalWrite( ledPin, LOW);        // иначе на 12м 0 (он выключен)
+    digitalWrite( ledredPin, LOW);        // иначе на 12м 0 (он выключен)
+    digitalWrite(ledbluePin, LOW);
     noTone(activBuz);  
   }
     
